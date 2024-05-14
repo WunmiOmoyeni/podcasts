3d2c2bc73059912fc025ee885e90defe0eb2f3fc
WunmiOmoyeni <omoyeniwunmi@gmail.com>
2024-05-14 14:59:58 -0700

    gege

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div className='flex bg-green'>
+        <h1>Logo</h1>
+        <p>Welcome</p>
+    </div>
+  )
+}
+
+export default Navbar;
